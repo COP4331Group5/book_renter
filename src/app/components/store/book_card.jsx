f6fbb44830b579fe0fdcf7df7e216112a400c6a3
sharonsajipaul <sharonsajipaul@gmail.com>
2024-03-26 21:05:57 +0000

    Add login redirection on Rent Now button click

--- src/app/components/store/book_card.jsx
+++ src/app/components/store/book_card.jsx
@@ -1,17 +1,25 @@
-
+import Link from 'next/link';
+import { redirect, useRouter } from 'next/navigation';
 
 export default function BookCard({
     data
 }) {
+    const router = useRouter();
+
+    const goToLogin = () => {
+        console.log("Redirecting to login page");
+        redirect(router, "/login");
+    }
+
     return (
         <div className="flex flex-col gap-2 h-fit w-[200px] bg-gray-800 m-2 p-4 rounded-md">
             <img className="max-w-[120px]" src={data.image} />
             <div>
-            <p className="font-bold">{data.name}</p>
-            <p className="text-slate-300">{data.author}</p>
+                <p className="font-bold">{data.name}</p>
+                <p className="text-slate-300">{data.author}</p>
             </div>
             <div className="flex-grow" />
-            <button className="bg-blue-300">Rent Now</button>
+            <Link href={"/login"} className="bg-slate-600 rounded-md p-2 text-sm">Rent Now</Link>
         </div>
-    )    
+    )
 }
